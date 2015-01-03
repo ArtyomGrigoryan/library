7040e021ea2968575368c5161c4b654cc7a669b5
FranzCrawl <artemg93@mail.ru>
2015-01-03 22:08:33 +0400

    Update mail.php

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => 'artyomg93@gmail.com', 'name' => 'Artyom'),
+	'from' => array('address' => 'lol', 'name' => 'Artyom'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => 'artyomg93@gmail.com',
+	'username' => 'lol',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => 'Gulshevich',
+	'password' => 'lol',
 
 	/*
 	|--------------------------------------------------------------------------
